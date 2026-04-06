14984fc880666caaf567b959cc40e26aad0ccd3c
Dana Gunawan <dewakadekdana196@gmail.com>
2026-04-07 07:35:40 +0800

    vite module

---
+++ chatbot/src/components/ChatMessages.jsx
@@ -0,0 +1,62 @@
+
+import { useEffect,useRef } from "react";
+import RobotProfileImage from "../assets/bot.png";
+import UserProfileImage from "../assets/user.png";
+
+function ChatMessage({ message, sender }) {
+  // const message = props.message;
+  // const sender = props.sender;
+  // const { message, sender } = props;
+
+  /*
+        if (sender === 'robot') {
+          return (
+            <div>
+              <img src="robot.png" width="50" />
+              {message}
+            </div>
+          );
+        }
+        */
+
+  return (
+    <div
+      className={sender === "user" ? "chat-message-user" : "chat-message-robot"}
+    >
+      {sender === "robot" && (
+        <img src={RobotProfileImage} className="chat-message-profile" />
+      )}
+      <div className="chat-message-text">{message}</div>
+      {sender === "user" && (
+        <img src={UserProfileImage} className="chat-message-profile" />
+      )}
+    </div>
+  );
+}
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = chatMessagesRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [chatMessages]);
+
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default ChatMessages;
