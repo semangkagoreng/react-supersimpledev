6d7402d325dfd0a054b6f6bed44d97b9adc0eb92
Dana Gunawan <semangkagoreng30@gmail.com>
2026-04-11 11:25:33 +0800

    e-commerce

---
+++ e-commerce_project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Header } from './components/Header'
+import Homepage from './pages/Homepage'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Homepage />
+    </>
+  )
+}
+
+
+export default App
